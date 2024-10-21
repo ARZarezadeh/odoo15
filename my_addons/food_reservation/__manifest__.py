920add7b4026ed727ca487b122e2c9e645dd3215
ARZarezadeh <alirezazz.abar@gmail.com>
2024-10-21 16:42:13 +0330

    implement access roles and rules for food reservation

--- my_addons/food_reservation/__manifest__.py
+++ my_addons/food_reservation/__manifest__.py
@@ -5,7 +5,7 @@
     "application": "True",
     "license": "LGPL-3",
     "data": [
-        # "security/food_security.xml",
+        "security/food_security.xml",
         "security/ir.model.access.csv",
         "views/food_week_views.xml",
         "views/food_order_views.xml",
